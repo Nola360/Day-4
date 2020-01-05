ea4ec12a986172cdd8e5117b2ca7230bcc7bed9a
nola360 <adaramolajr@gmail.com>
2020-01-04 23:49:00 -0500

    added js content

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ const person = {
   team: 'New England Patriots',
   address: {
     city: "Foxboro",
-    state: "MA"
+    state: "MA",
   },
 
+  titles: '6 Super Bowl rings',
+
   getBirthYear: function () {
-    return 1987;
+    return 2020 - this.age;
   }
 }
 
@@ -37,3 +39,28 @@ console.log(val);
 
 val = person.position[0];
 console.log(val)
+
+val = person.titles;
+console.log(val);
+
+
+val = person.address.city;
+console.log(val);
+
+val = person.address.state;
+console.log(val);
+
+val = person.getBirthYear();
+console.log(val);
+
+// Arrays of Objects
+
+const people = [
+  { name: 'Steven', age: 44 },
+  { name: 'A. Smith', age: 54 },
+  { name: 'Molly Q', age: 40 },
+];
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i].name);
+}
